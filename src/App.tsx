fb2e931e988bfee7aa5d29ccb84fa87ddfda8483
nikkoscribblo <ediblechocolatething@gmail.com>
2024-09-11 01:16:05 +0700

    init: New version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Home } from "./pages/home"
+
 function App() {
-    return <></>
+    return (
+        <div className="mx-auto w-full sm:max-w-2xl">
+            <Home />
+        </div>
+    )
 }
 
 export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+    return (
+        <div className="grid grid-cols-4">
+            <div className="col-span-4">
+                <h1>Hi there! I&apos;m Berry, nice to meet ya!</h1>
+            </div>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+        </div>
+    )
+}
